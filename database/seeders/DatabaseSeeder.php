32a142f33c9392c8a3c9096d873bfd8a295cce67
MarsianinM <shevamakc@gmail.com>
2021-05-08 16:52:42 +0300

    Change sedd file Database 3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use DB;
+use Hash;
+use Illuminate\Database\Eloquent\Model;
 use Modules\Users\Entities\User;
 use Illuminate\Database\Seeder;
 
@@ -15,16 +17,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
         //$this->call(RoleSeeder::class);
         //$this->call(SettingSeeder::class);
-        DB::table('users')->insert([
+        DB::table('users')->insert(
             [
                 'name' => 'Тест',
                 'email' => 'testaccount@smm.ua',
-                'password' => \Hash::make('11111111'),
+                'password' => '$argon2id$v=19$m=1024,t=2,p=2$TEQyWjBZcGVkWWZLN2JpTA$/HVhobcctdWTwdhNNPIp0EJRWFfuY1O2j1w+g+S4vjc',//Hash::make('11111111'),
                 'email_verified_at' => now(),
             ]
-            ]);
+            );
         /*User::create([
             'name' => 'Тест',
             'email' => 'testaccount@smm.ua',
